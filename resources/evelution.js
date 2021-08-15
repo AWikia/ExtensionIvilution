749547759174709d2f7e5b727298b7fdd655392e
HM The Mister Techno <giorgos4006@gmail.com>
2021-08-16 02:11:59 +0300

    Add files via upload

---
+++ resources/evelution.js
@@ -0,0 +1,8 @@
+( function () {
+
+	/**
+	 * @class mw.boilerPlate
+	 * @singleton
+	 */
+	console.log("Ivilution extension did not load as the official CPE library is provided by the skin")
+}() );
