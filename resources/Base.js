30e56943afc467709a100c06fd76f9eb5e2a38af
HM The Mister Techno <giorgos4006@gmail.com>
2021-08-16 02:25:15 +0300

    Add files via upload

--- resources/Base.js
+++ resources/Base.js
@@ -4,8 +4,6 @@
 	 * @class mw.boilerPlate
 	 * @singleton
 	 */
-	mw.Ivilution = {
-	};
 	$('html').addClass("ivilution-loaded");
 	var body_classes = $('body').attr('class').split(' ');
 	for (let i = 0; i < body_classes.length; i++) {
